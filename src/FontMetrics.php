26804fd0732b9ff5e5c250d834fc8b3c9d2d7be6
Ajay Kumar Choudhary <ajay@froiden.com>
2023-01-30 22:37:33 +0530

    Update FontMetrics.php

--- src/FontMetrics.php
+++ src/FontMetrics.php
@@ -229,7 +229,7 @@ class FontMetrics
             return false;
         }
 
-        $localTempFile = @tempnam(config('dompdf.options.temp_dir'), "dompdf-font-");
+        $localTempFile = @tempnam(storage_path('tmp'), "dompdf-font-");
         file_put_contents($localTempFile, $remoteFileContent);
 
         $font = Font::load($localTempFile);
